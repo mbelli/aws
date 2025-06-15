209de39f6b38160a9a7292f1b608de043c6ec16c
Thierry Mbelli <thierry@rifumoholdings.co.za>
2025-06-15 21:21:45 +0200

    update key

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "test-bucket"
     region = "us-east-1"
-    key = "github/test.tfstate"
+    key = "github/terraform.tfstate"
     encrypt = true
   }
   required_version = ">=0.13.0"
