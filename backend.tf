d5fed9d366245d56247d7acf4f6ce80a0fbcb6dd
Thierry Mbelli <thierry@rifumoholdings.co.za>
2025-06-15 07:47:19 +0200

    my commit

---
+++ backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "test-bucket"
+    region = "us-east-1"
+    key = "github/test.tfstate"
+    encrypt = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">=2.7.0"
+      source = "hashicorp/aws"
+    }
+  }
+}
